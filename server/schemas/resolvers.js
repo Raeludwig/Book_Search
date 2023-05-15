796e4c6fb8fc8801b6de3a15fce5b48d9b728dac
Raeludwig <racheleludwig@gmail.com>
2023-05-15 14:06:59 -0600

    setting up schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,33 @@
+const { User, Book } = require("../models");
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return userData;
+      }
+        throw new AuthenticationError("Not logged in");
+    },
+  },
+//   Mutation: {
+//     createMatchup: async (parent, args) => {
+//       const matchup = await Matchup.create(args);
+//       return matchup;
+//     },
+//     createVote: async (parent, { _id, techNum }) => {
+//       const vote = await Matchup.findOneAndUpdate(
+//         { _id },
+//         { $inc: { [`tech${techNum}_votes`]: 1 } },
+//         { new: true }
+//       );
+//       return vote;
+//     },
+//   },
+};
+
+module.exports = resolvers;
