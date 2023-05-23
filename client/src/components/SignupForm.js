c083ee584e90bffdd83407bd3b00efbe96f161a6
Raeludwig <racheleludwig@gmail.com>
2023-05-23 14:07:10 -0600

    updated signupForm.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,16 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
-import Auth from '../utils/auth';
+import { useMutation } from '../utils/API';
+import {ADD_USER} from '../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [userFormData, setUserFormData] = useState({ 
+    username: '',
+     email: '', 
+     password: '' 
+    });
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser] = useMutation(ADD_USER);
+
+useEffect(() => {
+  if (error) {
+    setShowAlert(true);
+  } else {
+    setShowAlert(false);
+  }
+}, [error]);
+
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,19 +43,15 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+const {data} = await addUser({
+  variables: {...userFormData},
+});
+console.log(data);
+Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
-      setShowAlert(true);
     }
+
 
     setUserFormData({
       username: '',
@@ -48,6 +59,8 @@ const SignupForm = () => {
       password: '',
     });
   };
+
+  
 
   return (
     <>
