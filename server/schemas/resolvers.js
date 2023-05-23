20d4d8f11426b243b70cbad8bcc8b2598e0f6db6
Raeludwig <racheleludwig@gmail.com>
2023-05-23 10:19:52 -0600

    updated resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,20 +14,48 @@ const resolvers = {
         throw new AuthenticationError("Not logged in");
     },
   },
-//   Mutation: {
-//     createMatchup: async (parent, args) => {
-//       const matchup = await Matchup.create(args);
-//       return matchup;
-//     },
-//     createVote: async (parent, { _id, techNum }) => {
-//       const vote = await Matchup.findOneAndUpdate(
-//         { _id },
-//         { $inc: { [`tech${techNum}_votes`]: 1 } },
-//         { new: true }
-//       );
-//       return vote;
-//     },
-//   },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return {token, user};
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne( {email });
+    
+      if (!user){
+        throw new AuthenticationError('Incorrect credentials')
+      }
+
+      const correctPw=await user.isCorrectPassword(password);
+
+      if(!correctPw){
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token =signToken(user);
+      return {token, user};
+    },
+    saveBook:async (parent, {bookData}, context) =>{
+      if (context.user){
+        const updatedUser =await User.findByIdAndUpdate(
+          {_id: context.user._id},
+          {$push: {savedBooks: bookData}},
+          {new:true}
+        );
+
+        return updatedUser;
+      }
+    },
+
+    removeBook: async (parent, {bookData}, context) => {
+      if (context.user){
+        const u
+      }
+    }
+
+  },
 };
 
 module.exports = resolvers;
